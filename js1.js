410db6a730a060f77a72c8d19b9668592287847e
Ponraj <110389942+Ponraj-dev@users.noreply.github.com>
2023-01-11 21:55:45 +0530

    Update js1.js

--- js1.js
+++ js1.js
@@ -145,7 +145,7 @@ function weather(){
 
 window.addEventListener("load",()=>{
     
-    //music();
+    music();
     speak("Initiating system");
     speak("Activating jarvis");
     wishMe();
@@ -168,10 +168,10 @@ window.addEventListener("load",()=>{
 
 const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 const recognition = new SpeechRecognition();                                                  //assign a varibable to speech recoginition
-recognition.continous = false;                                     //set the mic to false default ,so it will turn on only if  we call
-recognition.lang = "en-US";                                       //set languange
-recognition.interimResult=false;                                    //to get the word accuracy
-recognition.maxAlternative=1;
+//recognition.continous = false;                                     //set the mic to false default ,so it will turn on only if  we call
+//recognition.lang = "en-US";                                       //set languange
+//recognition.interimResult=false;                                    //to get the word accuracy
+//recognition.maxAlternative=1;
 
 recognition.onresult = (event) => {
     const current = event.resultIndex;
@@ -185,7 +185,6 @@ startButton.addEventListener('click', ()=>{
 
 
 
-
 function speakThis(message) {
     const speech = new SpeechSynthesisUtterance();
 
@@ -193,18 +192,97 @@ function speakThis(message) {
 
   
     if(message.includes('hey') || message.includes('hello')) {
+
+        document.getElementById("text1").innerHTML = message;
         const finalText = "Hello Boss";
         speech.text = finalText;
+        document.getElementById("text2").innerHTML = finalText;
         console.log(message);
+
+    }
+
+    else if(message.includes('how are you')) {
+        const finalText = "I am fine boss tell me how can i help you";
+        speech.text = finalText;
+    }
+
+    else if(message.includes('name')) {
+        const finalText = "My name is jarvis";
+        speech.text = finalText;
+    }
+
+    else if(message.includes('open google')) {
+        window.open("https://google.com", "_blank");
+        const finalText = "Opening Google";
+        speech.text = finalText;
+    }
+    else if(message.includes('what is weather') || message.includes('how is weather ')) {
+        speech.text = "please wait";
+        weather();
+        console.log(message);
+    } 
+    else if(message.includes('open instagram')) {
+        window.open("https://instagram.com", "_blank");
+        const finalText = "Opening instagram";
+        speech.text = finalText;
+    }
+   
+
+    else if(message.includes('what is') || message.includes('who is') || message.includes('what are')) {
+        window.open(`https://www.google.com/search?q=${message.replace(" ", "+")}`, "_blank");
+        const finalText = "This is what i found on internet regarding " + message;
+        speech.text = finalText;
+    }
+   
+
+    else if(message.includes('wikipedia')) {
+        window.open(`https://en.wikipedia.org/wiki/${message.replace("wikipedia", "")}`, "_blank");
+        const finalText = "This is what i found on wikipedia regarding " + message;
+        speech.text = finalText;
+    }
+
+    else if(message.includes('time')) {
+        const time = new Date().toLocaleString(undefined, {hour: "numeric", minute: "numeric"})
+        const finalText = time;
+        speech.text = finalText;
+    }
+
+    else if(message.includes('date')) {
+        const date = new Date().toLocaleString(undefined, {month: "short", day: "numeric"})
+        const finalText = date;
+        speech.text = finalText;
+    }
+
+    else if(message.includes('calculator')) {
+        window.open('Calculator:///')
+        const finalText = "Opening Calculator";
+        speech.text = finalText;
+    }
+        
+    else {
+        window.open(`https://www.google.com/search?q=${message.replace(" ", "+")}`, "_blank");
+        const finalText = "I found some information for " + message + " on google";
+        speech.text = finalText;
+    
     }
     speech.volume = 1;
     speech.pitch = 2;
     speech.rate = 1;
 
+
+
     window.speechSynthesis.speak(speech);
+
 }
 
 
+
+
+
+//module to speak general things ...............................................................................................................
+
+
+//module to get location........................................................................................................................
 
 // var div  = "location";
 // function getLocation() {
@@ -255,3 +333,4 @@ function speakThis(message) {
 
 // getLocation();
 // location(lat,lng);
+
